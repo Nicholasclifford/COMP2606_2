188e86c5b521e309be4510e4aa3e6e9eb86629a4
Admin <nicholasclifford05539@gmail.com>
2021-11-18 12:02:33 -0400

    start

--- app/src/main/java/com/example/product_management/TopLevelActivity.java
+++ app/src/main/java/com/example/product_management/TopLevelActivity.java
@@ -2,7 +2,10 @@ package com.example.product_management;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class TopLevelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button receive_button=findViewById(R.id.toprecievebutton);
+        receive_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(TopLevelActivity.this,sublevelActivity.class);
+                intent.putExtra(sublevelActivity.choice,true);
+                startActivity(intent);
+            }
+        });
+        Button ordering_button=findViewById(R.id.topsendbutton);
+        ordering_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(TopLevelActivity.this,sublevelActivity.class);
+                intent.putExtra(sublevelActivity.choice,false);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/product_management/sublevelActivity.java
+++ app/src/main/java/com/example/product_management/sublevelActivity.java
@@ -1,14 +1,39 @@
 package com.example.product_management;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class sublevelActivity extends AppCompatActivity {
 
+    public static String choice;
+    public boolean option;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sublevel);
+
+        Intent intent =getIntent();
+        option=intent.getExtras().getBoolean(choice);
+
+
+        if(option) {
+
+            FragmentManager fragmentManager=getSupportFragmentManager();
+            FragmentTransaction ft=fragmentManager.beginTransaction();
+            ft.replace(R.id.sublevel_frag, new ReceivingStocksFragment());
+            ft.commit();
+        }
+        else
+        {
+            FragmentManager fragmentManager=getSupportFragmentManager();
+            FragmentTransaction ft=fragmentManager.beginTransaction();
+            ft.replace(R.id.sublevel_frag, new OrderingStocksFragment());
+            ft.commit();
+        }
+
     }
 }
