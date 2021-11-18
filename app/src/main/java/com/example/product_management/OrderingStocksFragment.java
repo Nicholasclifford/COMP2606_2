43a61afb8463958775279c73c58ba128a97fc34a
Admin <nicholasclifford05539@gmail.com>
2021-11-17 20:19:28 -0400

    Start

--- app/src/main/java/com/example/product_management/OrderingStocksFragment.java
+++ app/src/main/java/com/example/product_management/OrderingStocksFragment.java
@@ -8,21 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OrderingStocksFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class OrderingStocksFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public OrderingStocksFragment() {
         // Required empty public constructor
@@ -33,10 +22,7 @@ public class OrderingStocksFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
